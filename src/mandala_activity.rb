a7c46c9c9df308ad3bc83320d17ca1560d115637
Jônatas Paganini <jonatasdp@gmail.com>
2014-05-25 01:19:29 -0300

    putting all mandalas from book 1

--- src/mandala_activity.rb
+++ src/mandala_activity.rb
@@ -9,19 +9,22 @@ end
 
 class MandalaActivity
   @@mandalas = [
-    $package::R.drawable.mandala_1_01b,
-    $package::R.drawable.mandala_1_02b,
-    $package::R.drawable.mandala_1_03b,
-    $package::R.drawable.mandala_1_04b
+    $package::R.drawable.mandala_1_01b, $package::R.drawable.mandala_1_02b, $package::R.drawable.mandala_1_03b, $package::R.drawable.mandala_1_04b,
+    $package::R.drawable.mandala_1_05b, $package::R.drawable.mandala_1_06b, $package::R.drawable.mandala_1_07b, $package::R.drawable.mandala_1_08b,
+    $package::R.drawable.mandala_1_09b, $package::R.drawable.mandala_1_010b, $package::R.drawable.mandala_1_011b, $package::R.drawable.mandala_1_012b,
+    $package::R.drawable.mandala_1_013b, $package::R.drawable.mandala_1_014b, $package::R.drawable.mandala_1_015b, $package::R.drawable.mandala_1_016b,
+    $package::R.drawable.mandala_1_017b, $package::R.drawable.mandala_1_018b, $package::R.drawable.mandala_1_019b, $package::R.drawable.mandala_1_020b,
+    $package::R.drawable.mandala_1_021b, $package::R.drawable.mandala_1_022b, $package::R.drawable.mandala_1_023b, $package::R.drawable.mandala_1_024b,
+    $package::R.drawable.mandala_1_025b, $package::R.drawable.mandala_1_026b
   ]
   def onCreate(bundle)
     super
-    set_title 'Mandalas Panel'
     @index = 0
-    self.content_view = linear_layout do
+    self.content_view = linear_layout id: "@+id/mainView" do
       @mandala = image_view :image_resource => @@mandalas[@index],
                  :scale_type => ImageView::ScaleType::FIT_CENTER,
                  :on_click_listener => proc{|v| show_next_mandala(v)},
+                 :on_touch_listener => proc{|view, motion_event|change_mandala_speed(view,motion_event)},
                  :layout => {
                    :weight= => 1,
                    :height= => :fill_parent,
@@ -34,11 +37,15 @@ class MandalaActivity
 
       @mandala.startAnimation(@animation);
     end
+
   rescue Exception
     puts "Exception creating activity: #{$!}"
     puts $!.backtrace.join("\n")
   end
   def show_next_mandala view
     @mandala.image_resource = @index < @@mandalas.size ? @@mandalas[@index += 1] : @@mandalas[@index = 0]
+  end
+  def change_mandala_speed v,event
+    android.util.Log.i "touch eventt from mandala", ">> #{v} => #{event}"
   end
 end
