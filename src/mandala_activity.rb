2388dcdfe7d55414c608d8793f4e724e53dc7e8d
Jônatas Paganini <jonatasdp@gmail.com>
2014-05-26 20:53:53 -0300

    adding buttons and accelerator

--- src/mandala_activity.rb
+++ src/mandala_activity.rb
@@ -1,7 +1,7 @@
 require 'ruboto/widget'
 require 'ruboto/util/toast'
 
-ruboto_import_widgets :Button, :LinearLayout, :TextView, :ImageView
+ruboto_import_widgets :Button, :LinearLayout, :TextView, :ImageView, :SeekBar
 %w{Animation RotateAnimation LinearInterpolator}.each do |_clazz|
   java_import "android.view.animation.#{_clazz}"
 end
@@ -20,33 +20,63 @@ class MandalaActivity
   def onCreate(bundle)
     super
     @index = 0
-    self.content_view = linear_layout do
-      @mandala = image_view :image_resource => @@mandalas[@index],
-                 :scale_type => ImageView::ScaleType::FIT_CENTER,
-                 :on_click_listener => proc{|v| show_next_mandala(v)},
-                 :on_touch_listener => proc{|view, motion_event|change_mandala_speed(view,motion_event)},
-                 :layout => {
-                   :weight= => 1,
-                   :height= => :fill_parent,
-                   :width= => :fill_parent}
+    self.content_view =
+      linear_layout :orientation => :vertical do
+        @mandala = image_view :image_resource => @@mandalas[@index],
+          :scale_type => ImageView::ScaleType::FIT_CENTER,
+#          :on_touch_listener => proc{|view, motion_event|change_mandala_speed(view,motion_event)},
+          :on_click_listener => proc{|v| pause_mandala(v) },
+          :layout => {
+            :weight= => 1,
+            :height= => :fill_parent,
+            :width= => :fill_parent}
+        
+        linear_layout do
+          button :text => "<", :on_click_listener => proc{|v| previous_mandala }
+          button :text => "=", :on_click_listener => proc{|v| pause_mandala(v) }
+          button :text => ">", :on_click_listener => proc{|v| next_mandala }
+          seek_bar :on_seek_bar_change => proc{|view, progress_changed| change_mandala_speed(view, progress_changed)},
+            :layout => { :height => :fill_parent, :width => :fill_parent}
+        end
 
-      @animation = RotateAnimation.new(0.0, 360.0,  RotateAnimation::RELATIVE_TO_SELF, 0.5, RotateAnimation::RELATIVE_TO_SELF, 0.5)
-      @animation.setInterpolator(LinearInterpolator.new())
-      @animation.setRepeatCount(Animation::INFINITE)
-      @animation.setDuration(500)
+        @animation = RotateAnimation.new(0.0, 360.0,  RotateAnimation::RELATIVE_TO_SELF, 0.5, RotateAnimation::RELATIVE_TO_SELF, 0.5)
+        @animation.setInterpolator LinearInterpolator.new()
+        @animation.setRepeatCount Animation::INFINITE
+        @animation.setDuration 500
 
-      @mandala.startAnimation(@animation);
-    end
+        @mandala.startAnimation @animation
+      end
 
   rescue Exception
     puts "Exception creating activity: #{$!}"
     puts $!.backtrace.join("\n")
   end
-  def show_next_mandala view
-    @mandala.image_resource = @index < @@mandalas.size ? @@mandalas[@index += 1] : @@mandalas[@index = 0]
+  def next_mandala
+    if @index < @@mandalas.size
+      @mandala.image_resource =  @@mandalas[@index += 1]
+    else
+      @@mandalas[@index = 0]
+    end
+  end
+  def pause_mandala v
+    if v.text == "="
+      v.text = "o"
+      @mandala.clearAnimation
+    else
+      v.text = "="
+      @mandala.startAnimation @animation
+    end
+  end
+  def previous_mandala
+    @mandala.image_resource =
+      if @index > 0
+        @@mandalas[@index -= 1]
+      else
+        @@mandalas[@index = @@mandalas.size - 1]
+      end
   end
   def change_mandala_speed v,event
-    @animation.setDuration(event.x+event.y*2)
-    toast "#{@animation.duration}ms por ciclo"
+    android.util.Log.info "change mandala speed",event.to_s
+    @animation.setDuration(event.progress / 1000)
   end
 end
